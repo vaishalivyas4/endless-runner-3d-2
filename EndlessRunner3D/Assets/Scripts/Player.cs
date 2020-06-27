8731587cfacbc7007e3dbf207294a1121fda6abb
Vaishali Vyas <vaishalivyas4@users.noreply.github.com>
2020-06-27 16:26:18 +0530

    Commit#1

---
+++ EndlessRunner3D/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+
+    Rigidbody rb;
+
+    public float xLimit = 2.4f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+
+        float h = Input.GetAxis("Horizontal");
+
+        Vector3 movement = transform.right * h * moveSpeed;
+        movement.y = rb.velocity.y;
+        rb.velocity = movement;
+
+        Vector3 curPos = transform.position;
+        curPos.x = Mathf.Clamp(curPos.x,-xLimit,xLimit);
+        transform.position = curPos;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Debug.Log("Game Over");
+        }else if (collision.gameObject.tag == "Ground")
+        {
+            //Enable Jump
+        }
+    }
+
+}
---
+++ EndlessRunner3D/Assets/Scripts/WorldManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour
+{
+
+    public GameObject ground;
+
+    GameObject obstaclePrefab;
+
+    public GameObject[] obsPrefabs;
+
+    public Transform spawnPoint1, spawnPoint2, spawnPoint3;
+
+    Transform spawnPoint;
+
+    public Transform belt;
+
+    public float maxBeltSpeed;
+
+    float beltSpeed;
+
+    int spawnPointSelector = 1;
+
+    int obsSelector = 0;
+
+    Material groundMat;
+
+    float curOffset = 0;
+    public float worldScrollSpeed = 5f;
+    public float groundScrollSpeed = 7.5f;
+
+    private void Start()
+    {
+        groundMat = ground.GetComponent<MeshRenderer>().material;
+        InvokeRepeating("GenerateObstacle", 0.5f, 2f);
+    }
+
+    void Update()
+    {
+        worldScrollSpeed += 0.001f;
+        beltSpeed = (worldScrollSpeed / 10);
+        beltSpeed = Mathf.Clamp(beltSpeed,0,maxBeltSpeed);
+        curOffset += (groundScrollSpeed/1000);
+        groundMat.mainTextureOffset = new Vector2(0,curOffset);
+        belt.Translate(transform.forward * beltSpeed);
+    }
+
+    void GenerateObstacle()
+    {
+        spawnPointSelector = Random.Range(1, 4);
+        if (spawnPointSelector == 1)
+        {
+            spawnPoint = spawnPoint1;
+        }else if (spawnPointSelector == 2)
+        {
+            spawnPoint = spawnPoint2;
+        }
+        else
+        {
+            spawnPoint = spawnPoint3;
+        }
+
+        obsSelector = Random.Range(0,obsPrefabs.Length);
+
+        obstaclePrefab = obsPrefabs[obsSelector];
+
+        if (obsSelector == 1)
+        {
+            obsSelector = Random.Range(0,2);
+
+            if(obsSelector == 1)
+            {
+                Instantiate(obstaclePrefab, spawnPoint.position, spawnPoint.rotation, belt);
+            }
+            else
+            {
+                Instantiate(obstaclePrefab, spawnPoint.position + Vector3.up, spawnPoint.rotation, belt);
+            }
+
+        }
+        else
+        {
+            Instantiate(obstaclePrefab, spawnPoint.position, spawnPoint.rotation, belt);
+        }
+
+        
+    }
+}
